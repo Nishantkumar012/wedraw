feeb929db5fa1d84c9e877f4c4ab09e2cb419ca6
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2026-01-23 01:16:36 +0530

    cors error

--- frontend/src/pages/Rooms.tsx
+++ frontend/src/pages/Rooms.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 type Room = {
   id: string;
