277ac6362f1122c14d2d3f5e34d1c7a1429c8510
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2026-05-11 14:30:05 +0530

    try 7

--- frontend2/src/services/api.ts
+++ frontend2/src/services/api.ts
@@ -8,7 +8,7 @@ export const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
         //ngrok ke liye
-        'ngrok-skip-browser-warning': 'true'
+        // 'ngrok-skip-browser-warning': 'true'
 
     },
 });
