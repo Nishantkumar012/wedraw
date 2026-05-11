fd44d1e1e914d3eeee6bca5ab5992a8e920a7336
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2026-05-11 15:44:14 +0530

    try 10

--- frontend2/src/services/api.ts
+++ frontend2/src/services/api.ts
@@ -8,6 +8,7 @@ export const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
         //ngrok ke liye
+         
         'ngrok-skip-browser-warning': 'true'
 
     },
